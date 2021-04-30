0c72fd2354d6696e9e34694020f95b9b222bca33
Eric Zhang <55414915+ezzhang8@users.noreply.github.com>
2021-04-29 21:15:25 -0700

    Began some endpoints, installed nodemon for workflow
    Adding words/viewing tree hierarchy now works, but not much use yet for autocomplete or deletion

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 const express = require('express');
+const fs = require('fs');
+const { Trie, TrieNode } = require('./trie.js');
+
 const app = express();
 
+const trie = new Trie(JSON.parse(fs.readFileSync('data.json', {encoding: 'utf8'})));
 
+app.post('/words/:word', (req, res) => {
+    try {
+        TrieNode.add(trie.root, req.params.word);
+        trie.save(); 
 
-const PORT = 3000 || process.env.PORT;
+        res.status(200);
+        res.send(JSON.stringify({
+            "success": true,
+            "word": req.params.word
+        }));
+    }
+    catch {
+        res.status(400);
+        res.send(JSON.stringify({
+            "success": false,
+            "error": "Could not be added to trie."
+        }));
+    }
+})
+
+app.get('/get/', (req, res) => {
+    res.send(JSON.stringify(trie));
+
+    console.log(TrieNode.find(trie.root, "test"));
+})
+
+app.post('/get/', (req, res) => {
+    res.send(JSON.stringify(trie));
+})
+
+const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- trie.js
+++ trie.js
@@ -1,7 +1,70 @@
-class Trie {
-    constructor(root, children) {
+const fs = require('fs');
 
+class Trie {
+    constructor(root) {
+        this.root = root;
+    }
+
+    save() {
+        fs.writeFile('data.json', JSON.stringify(this.root), (err) => {
+            if (err != null) 
+                return err;
+        });
     }
 }
 
-module.exports.Trie = Trie;
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isWord = false;
+    }
+    /**
+     * Recursively adds a word to the trie
+     * @param {TrieNode} path - Path to a TrieNode
+     * @param {String} word - Word
+     * @returns {void}
+     */
+    static add(path, word) {
+        if (word.length == 0) { 
+            path.isWord = true;
+            return;
+        }
+
+        if (path.children[word.charAt(0)] == undefined) {
+            path.children[word.charAt(0)] = new TrieNode();
+        }
+
+        TrieNode.add(path.children[word.charAt(0)], word.slice(1));    
+    }
+
+    /**
+     * 
+     * @param {*} path 
+     * @param {*} word
+     * @returns {Boolean} 
+     */
+    static find(path, word) {
+        let temp = word;
+        let lastNodePath;
+
+        for (let i=0; i<temp.length; i++) {
+            if (path.children[temp.charAt(i)] == undefined)  {
+                console.log(path)
+                return false;
+            }
+            else {
+                lastNodePath = path.children[temp.charAt(i)];
+            }
+        }
+
+        return true;
+    }
+
+
+
+}
+
+module.exports = {
+    Trie,
+    TrieNode
+};
